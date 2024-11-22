4a42823f9a2725ec662d51f58f683ab38467e144
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-11-22 11:08:11 +0530

    Update hooks.py

--- ikraft/hooks.py
+++ ikraft/hooks.py
@@ -129,9 +129,10 @@ app_license = "mit"
 # ---------------
 # Override standard doctype classes
 
-# override_doctype_class = {
+override_doctype_class = {
 # 	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+  "Salary Slip": "ikraft.custom_script.salary_slip.CustomSalarySlip",
+}
 
 # Document Events
 # ---------------
