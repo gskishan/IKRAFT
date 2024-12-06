cc6daa262c8aa2c72ff694068c71250a21681476
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-12-06 16:37:37 +0530

    Update employee_daily_checkin.js

--- ikraft/ikraft/report/employee_daily_checkin/employee_daily_checkin.js
+++ ikraft/ikraft/report/employee_daily_checkin/employee_daily_checkin.js
@@ -2,7 +2,11 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Employee Daily Checkin"] = {
-	"filters": [
-
+	"filters": [{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default":frappe.datetime.get_today(),
+		},
 	]
 };
