bd1a4a1136585fbb14839f17a8d1f9268dc0904e
G S Kishan <95840962+gskishan@users.noreply.github.com>
2025-03-29 21:26:10 +0530

    Update employee_daily_checkin.py

--- ikraft/ikraft/report/employee_daily_checkin/employee_daily_checkin.py
+++ ikraft/ikraft/report/employee_daily_checkin/employee_daily_checkin.py
@@ -10,25 +10,27 @@ def execute(filters=None):
 	return columns, data
 
 def data_condtion(filters):
-    if filters:
-        condition = ""
+	if filters:
+		condition = ""
 
-        if filters.get("from_date") and filters.get("to_date"):
-            condition = "AND DATE(ec_in.time) BETWEEN '{0}' AND '{1}' ".format(
-                filters.get("from_date"), filters.get("to_date")
-            )
-        elif filters.get("from_date"):
-            condition = "AND DATE(ec_in.time) = '{0}' ".format(filters.get("from_date"))
+		if filters.get("from_date") and filters.get("to_date"):
+			condition = "AND DATE(ec_in.time) BETWEEN '{0}' AND '{1}' ".format(
+				filters.get("from_date"), filters.get("to_date")
+			)
+		elif filters.get("from_date"):
+			condition = "AND DATE(ec_in.time) = '{0}' ".format(filters.get("from_date"))
 
-        if filters.get("employee_name"):
-            condition += "AND LOWER(emp.employee_name) LIKE LOWER('%{0}%') ".format(
-                filters.get("employee_name")
-            )
-    else:
-        today = datetime.now().strftime('%Y-%m-%d')
-        condition = "AND DATE(ec_in.time) = '{0}' ".format(today)
+		if filters.get("employee_name"):
+			condition += "AND LOWER(emp.employee_name) LIKE LOWER('%{0}%') ".format(
+				filters.get("employee_name")
+			)
+	else:
+		today = datetime.now().strftime('%Y-%m-%d')
+		condition = "AND DATE(ec_in.time) = '{0}' ".format(today)
+	condition += "AND ec_in.shift = 'General'"
 
-    return condition
+
+	return condition
 
 
 		
@@ -36,36 +38,36 @@ def data_condtion(filters):
 
 
 def get_data(filters):
-    yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
-    cond = data_condtion(filters)
-    sql = """
+	yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+	cond = data_condtion(filters)
+	sql = """
 			  SELECT 
-			    ec_in.employee,
-			    emp.employee_name,
-			    DATE_FORMAT(ec_in.time, '%d-%m-%Y %H:%i:%s') AS "check_in_time",
-			    DATE_FORMAT(ec_out.time, '%d-%m-%Y %H:%i:%s') AS "check_out_time",
-			    ec_out.name AS check_out_id,
-			    ec_in.name AS check_in_id,
-			    ec_out.is_auto_created AS "is_auto_created"
+				ec_in.employee,
+				emp.employee_name,
+				DATE_FORMAT(ec_in.time, '%d-%m-%Y %H:%i:%s') AS "check_in_time",
+				DATE_FORMAT(ec_out.time, '%d-%m-%Y %H:%i:%s') AS "check_out_time",
+				ec_out.name AS check_out_id,
+				ec_in.name AS check_in_id,
+				ec_out.is_auto_created AS "is_auto_created"
 			FROM 
-			    `tabEmployee Checkin` ec_in
+				`tabEmployee Checkin` ec_in
 			LEFT JOIN 
-			    `tabEmployee Checkin` ec_out 
-			    ON ec_in.employee = ec_out.employee 
-			    AND DATE(ec_out.time) = DATE(ec_in.time) 
-			    AND ec_out.log_type = 'OUT'
+				`tabEmployee Checkin` ec_out 
+				ON ec_in.employee = ec_out.employee 
+				AND DATE(ec_out.time) = DATE(ec_in.time) 
+				AND ec_out.log_type = 'OUT'
 			LEFT JOIN 
-			    `tabEmployee` emp ON ec_in.employee = emp.name
+				`tabEmployee` emp ON ec_in.employee = emp.name
 			WHERE 
-			    ec_in.log_type = 'IN'
-			    {0}
+				ec_in.log_type = 'IN'
+				{0}
 			ORDER BY 
-			    ec_in.employee, ec_in.time;
+				ec_in.employee, ec_in.time;
 
-          """.format(cond)
-    frappe.errprint(sql)
-    frappe.log_error("SQL Query Execution", sql)
-    return frappe.db.sql(sql, as_dict=1)
+		  """.format(cond)
+	frappe.errprint(sql)
+	frappe.log_error("SQL Query Execution", sql)
+	return frappe.db.sql(sql, as_dict=1)
 
 
 def get_columns():
@@ -74,11 +76,11 @@ def get_columns():
 	columns+= [
 		{
 	 		'fieldname': 'employee',
-            'label':('Employee'),
-            'fieldtype': 'Link',
-            'options': 'Employee',
+			'label':('Employee'),
+			'fieldtype': 'Link',
+			'options': 'Employee',
 			'width': 200
-        },
+		},
 		{
 			'label': _('Employee Name'),
 			'fieldname': "employee_name",
@@ -98,12 +100,12 @@ def get_columns():
 			 'options': 'Employee Checkin',
 			'width': 200
 		},
-        {
-            'fieldname': "check_out_time",
-            'label': ('Check Out Time'),
-            'fieldtype': 'Data',
+		{
+			'fieldname': "check_out_time",
+			'label': ('Check Out Time'),
+			'fieldtype': 'Data',
 			'width': 200
-        },
+		},
 		{
 			'label': _('Check-Out'),
 			'fieldname': "check_out_id",
@@ -117,12 +119,12 @@ def get_columns():
 			'fieldtype': 'Data',
 			'width': 160
 		},
-        {
-            'fieldname': "is_auto_created",
-            'label': ('Is Auto Created'),
-            'fieldtype': 'Check',
+		{
+			'fieldname': "is_auto_created",
+			'label': ('Is Auto Created'),
+			'fieldtype': 'Check',
 			'width': 60
-        }
+		}
 		
 	]
 	return columns
