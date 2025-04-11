ac5ba417aa77cdb06df18612417ce3e868156f1c
gskishan <gskishan>
2025-04-11 07:55:39 +0000

    shift wisee

--- ikraft/ikraft/report/employee_daily_checkin_shift_wise/employee_daily_checkin_shift_wise.js
+++ ikraft/ikraft/report/employee_daily_checkin_shift_wise/employee_daily_checkin_shift_wise.js
@@ -2,34 +2,34 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Employee Daily Checkin Shift Wise"] = {
-"filters": [{
-			"fieldname":"from_date",
-			"label": __("Date"),
-			"fieldtype": "Date",
-			"default":frappe.datetime.add_days(frappe.datetime.get_today(), -1),
+	"filters": [{
+		"fieldname": "from_date",
+		"label": __("Date"),
+		"fieldtype": "Date",
+		"default": frappe.datetime.add_days(frappe.datetime.get_today(), -1),
 
-		},
+	},
+	{
+		"fieldname": "to_date",
+		"label": __("To Date"),
+		"fieldtype": "Date",
+		"width": 80,
+		"default": frappe.datetime.add_days(frappe.datetime.get_today(), -1),
+		// "default": dateutil.year_end()
+	}, {
+		"fieldname": "shift",
+		"label": __("Shift"),
+		"fieldtype": "Link",
+		"options": "Shift Type",
+		"width": 80,
+		"reqd": 0,
+	},
 		{
-			"fieldname":"to_date",
-			"label": __("To Date"),
-			"fieldtype": "Date",
-			"width": 80,
-			"default":frappe.datetime.add_days(frappe.datetime.get_today(), -1),
-			// "default": dateutil.year_end()
-		},{
-			"fieldname":"shift",
-			"label": __("Shift"),
-			"fieldtype": "Link",
-			"options": "Shift Type",
-			"width": 80,
-			"reqd":0,
-		}
-		{
-			"fieldname":"employee_name",
-			"label": __("Employee Name"),
-			"fieldtype": "Data",
-			"width": 80,
-			"reqd":0,
-		}
+		"fieldname": "employee_name",
+		"label": __("Employee Name"),
+		"fieldtype": "Data",
+		"width": 80,
+		"reqd": 0,
+	}
 	],
 };
